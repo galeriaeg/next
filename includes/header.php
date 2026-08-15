fb9bf1f1b9be5fae3e85a997b6dd7759ab06ecd9
Alexandre <galeriaeg@gmail.com>
2026-08-15 02:25:57 -0300

    ajuste no menu

--- includes/header.php
+++ includes/header.php
@@ -9,13 +9,16 @@
     </button>
   </div>
   <div class="box-menu-lupa-mob">
-    <span class="icone-lupa-mob">
+    <!-- span class="icone-lupa-mob">
       <img src="public/imgs/btn-mob-busca.png" id="btnBusca" alt="buscaoff" onclick="controlaBuscaMob(this);" class="icone-lupa">
-    </span>
+    </span-->
     <span class="icone-lupa-mob">
       <img src="public/imgs/btn-bars-mob.png" id="btnMenu" alt="menuoff" class="icone-lupa" onclick="controlaMenuMob(this);">
     </span>
   </div>
 </section>
+
+
+<!--div id="alerta" class="alert-modal-info">Alerta busca desk</div-->
 
 <script src="public/js/header.js"></script>
